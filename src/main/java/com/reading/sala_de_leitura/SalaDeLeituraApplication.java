8f5172420120f29c86d85de46a4a2e1a3216b231
Taylam Moura <tay.moura@hotmail.com>
2025-01-18 16:41:37 -0300

    primeiro commit

---
+++ src/main/java/com/reading/sala_de_leitura/SalaDeLeituraApplication.java
@@ -0,0 +1,13 @@
+package com.reading.sala_de_leitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeLeituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeLeituraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reading/sala_de_leitura/SalaDeLeituraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reading.sala_de_leitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeLeituraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
