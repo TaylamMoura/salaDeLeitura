eb8ceb1f161039b5a1cafe5369cdac1b8d93e5a8
Taylam Moura <tay.moura@hotmail.com>
2025-01-30 12:15:03 -0300

    Alterar indentação

--- src/main/java/com/reading/sala_de_leitura/dto/LivroDTO.java
+++ src/main/java/com/reading/sala_de_leitura/dto/LivroDTO.java
@@ -2,14 +2,15 @@ package com.reading.sala_de_leitura.dto;
 
 import com.reading.sala_de_leitura.entity.Livro;
 
-public record LivroDTO(Long id,
-                       String titulo,
-                       String autor,
-                       int paginas,
-                       String urlCapa,
-                       int anoPublicacao) {
+public record LivroDTO(
+        Long id,
+        String titulo,
+        String autor,
+        int paginas,
+        String urlCapa,
+        int anoPublicacao) {
 
-    public LivroDTO(Livro livro){
+    public LivroDTO(Livro livro) {
         this(livro.getId(), livro.getTitulo(), livro.getAutor(), livro.getPaginas(), livro.getUrlCapa(), livro.getAnoPublicacao());
     }
 }
