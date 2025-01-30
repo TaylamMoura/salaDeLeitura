396a79df0c0c9d6f6892096ca08a0feadc77f5e0
Taylam Moura <tay.moura@hotmail.com>
2025-01-30 12:14:07 -0300

    Adicionar validadores nos métodos.

--- src/main/java/com/reading/sala_de_leitura/controller/LivroController.java
+++ src/main/java/com/reading/sala_de_leitura/controller/LivroController.java
@@ -2,8 +2,10 @@ package com.reading.sala_de_leitura.controller;
 
 
 import com.reading.sala_de_leitura.dto.LivroDTO;
-import com.reading.sala_de_leitura.entity.Livro;
-import com.reading.sala_de_leitura.service.APIGoogleBooksService;
+import com.reading.sala_de_leitura.service.LivroService;
+import com.reading.sala_de_leitura.service.validation.Validador;
+import com.reading.sala_de_leitura.service.validation.ValidadorExistencia;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +13,33 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class LivroController {
 
-    private APIGoogleBooksService service;
+    private LivroService service;
 
     @Autowired
-    public  LivroController(APIGoogleBooksService service){
+    private ValidadorExistencia<LivroDTO> validadorAdicionarNoBD;
+
+    @Autowired
+    private Validador<LivroDTO> validadorDeBusca;
+
+    @Autowired
+    public  LivroController(LivroService service){
         this.service = service;
     }
 
 
     @GetMapping("/pesquisarLivro")
     public LivroDTO pesquisarLivro(@RequestParam  String titulo){
+        LivroDTO livroDTO = new LivroDTO(null, titulo, null, 0, null, 0);
+        validadorDeBusca.validar(livroDTO);
         return service.pesquisarPorTitulo(titulo);
     }
 
+
     @Transactional
     @PostMapping("/salvarLivro")
-    public  String salvarLivro(@RequestBody LivroDTO livroDTO){
-        Livro livro = new Livro(livroDTO.id(), livroDTO.titulo(), livroDTO.autor(), livroDTO.paginas(), livroDTO.urlCapa(), livroDTO.anoPublicacao());
-        service.salvarLivro(livro);
-        return "Livro salvo!";
+    public  String salvarLivro(@RequestBody LivroDTO livroDTO) {
+        service.salvarLivro(livroDTO);
+        return "Livro salvo com sucesso";
     }
 
 }
