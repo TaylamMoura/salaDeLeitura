38266bde72c8b53e0db5fea6350042e1cc1aad53
Taylam Moura <tay.moura@hotmail.com>
2025-02-15 18:10:36 -0300

    Adicionado rota excluirLivro

--- src/main/java/com/reading/sala_de_leitura/controller/LivroController.java
+++ src/main/java/com/reading/sala_de_leitura/controller/LivroController.java
@@ -2,11 +2,11 @@ package com.reading.sala_de_leitura.controller;
 
 
 import com.reading.sala_de_leitura.dto.LivroDTO;
+import com.reading.sala_de_leitura.entity.Livro;
+import com.reading.sala_de_leitura.service.AtualizarLivro;
 import com.reading.sala_de_leitura.service.LivroService;
-import com.reading.sala_de_leitura.service.validation.Validador;
-import com.reading.sala_de_leitura.service.validation.ValidadorExistencia;
-import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -18,36 +18,71 @@ public class LivroController {
 
     private LivroService service;
 
-    @Autowired
-    private ValidadorExistencia<LivroDTO> validadorAdicionarNoBD;
+//    @Autowired
+//    private ValidadorExistencia<LivroDTO> validadorAdicionarNoBD;
+//
+//    @Autowired
+//    private Validador<LivroDTO> validadorDeBusca;
 
     @Autowired
-    private Validador<LivroDTO> validadorDeBusca;
-
-    @Autowired
-    public  LivroController(LivroService service){
+    public LivroController(LivroService service) {
         this.service = service;
     }
 
 
     @GetMapping("/pesquisarLivro")
-    public LivroDTO pesquisarLivro(@RequestParam  String titulo){
+    public LivroDTO pesquisarLivro(@RequestParam String titulo) {
         LivroDTO livroDTO = new LivroDTO(null, titulo, null, 0, null, 0);
-        validadorDeBusca.validar(livroDTO);
+//        validadorDeBusca.validar(livroDTO);
         return service.pesquisarPorTitulo(titulo);
     }
 
 
     @Transactional
     @PostMapping("/salvarLivro")
-    public ResponseEntity<String> salvarLivro(@RequestBody LivroDTO livroDTO) {
-        service.salvarLivro(livroDTO);
-        return ResponseEntity.ok("Livro salvo com sucesso");
+    public ResponseEntity<LivroDTO> salvarLivro(@RequestBody LivroDTO livroDTO) {
+        try {
+            service.salvarLivro(livroDTO);
+            return ResponseEntity.ok(livroDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/livrosSalvos")
-    public List<LivroDTO> exibirLivros(){
+    public List<LivroDTO> exibirLivros() {
         return service.exibirLivrosSalvos();
     }
 
+    @Transactional
+    @DeleteMapping("/excluirLivro/{id}")
+    public ResponseEntity<Void> excluirLivro(@PathVariable Long id) {
+        try {
+            service.deletarLivro(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Transactional
+    @PutMapping("/editarLivro/{id}")
+    public ResponseEntity<LivroDTO> editarLivro(@PathVariable Long id, @RequestBody AtualizarLivro atualizarDados){
+        try{
+            Livro livroAtualizado = service.editarLivro(id, atualizarDados);
+            LivroDTO livroDTO = new LivroDTO(
+                    livroAtualizado.getId(),
+                    livroAtualizado.getTitulo(),
+                    livroAtualizado.getAutor(),
+                    livroAtualizado.getPaginas(),
+                    livroAtualizado.getUrlCapa(),
+                    livroAtualizado.getAnoPublicacao()
+            );
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
+
+
