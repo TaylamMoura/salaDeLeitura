e8d70c2e237c546cf683a2825bee580d914c4680
Taylam Moura <tay.moura@hotmail.com>
2025-02-17 16:01:14 -0300

    teste

--- src/main/java/com/reading/sala_de_leitura/service/validation/ValidadorDeBusca.java
+++ src/main/java/com/reading/sala_de_leitura/service/validation/ValidadorDeBusca.java
@@ -1,24 +1,24 @@
-package com.reading.sala_de_leitura.service.validation;
-
-import com.reading.sala_de_leitura.dto.LivroDTO;
-import jakarta.validation.ValidationException;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ValidadorDeBusca implements Validador<LivroDTO>{
-
-    @Override
-    public void validar(LivroDTO dados) {
-        if (dados == null){
-            throw new ValidationException("Dados do livro são nulos");
-        }
-
-        var titulo = dados.titulo();
-        if (titulo == null || titulo.trim().isEmpty()){
-            throw new ValidationException("Titulo incorreto. O título não pode ser nulo ou vazio.");
-        }
-    }
-}
+//package com.reading.sala_de_leitura.service.validation;
+//
+//import com.reading.sala_de_leitura.dto.LivroDTO;
+//import jakarta.validation.ValidationException;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class ValidadorDeBusca implements Validador<LivroDTO>{
+//
+//    @Override
+//    public void validar(LivroDTO dados) {
+//        if (dados == null){
+//            throw new ValidationException("Dados do livro são nulos");
+//        }
+//
+//        var titulo = dados.titulo();
+//        if (titulo == null || titulo.trim().isEmpty()){
+//            throw new ValidationException("Titulo incorreto. O título não pode ser nulo ou vazio.");
+//        }
+//    }
+//}
 
 // este validador verificar se o objeto LivroDTO não é nulo
 //e verifica se o titulo é nulo ou vazio.
