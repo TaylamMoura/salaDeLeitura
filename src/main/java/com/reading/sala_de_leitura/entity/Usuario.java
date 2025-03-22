28990bb4437f94b68a0208f8cb9dcd216493c7db
Taylam Moura <tay.moura@hotmail.com>
2025-03-22 13:12:48 -0300

    Adicionado atributos nomes, dataAniversario, livros e adição de validações nos atributos

--- src/main/java/com/reading/sala_de_leitura/entity/Usuario.java
+++ src/main/java/com/reading/sala_de_leitura/entity/Usuario.java
@@ -1,9 +1,16 @@
 package com.reading.sala_de_leitura.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios")
@@ -19,14 +26,29 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @NotEmpty(message = "Defina um nome de usuário!")
     private String usuario;
 
+    @NotEmpty(message = "O email é obrigatório!")
+    @Email(message = "Escreva um email válido!")
     private String email;
 
+    @NotEmpty(message = "Defina uma senha.")
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 números")
     @Column(name = "senha_hash")
-    private String senhaHash;
+    private String senha;
 
-    @Column(name = "data_criacao")
+    @Column(name = "data_criacao", updatable = false)
+    @CreationTimestamp
     private Timestamp dataCriacao;
+
+    @NotEmpty(message = "Digite seu nome!")
+    private String nome;
+
+    @Column(name = "data_aniversario")
+    @NotEmpty(message = "Digite sua data de aniversário.")
+    private Date dataAniversario;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Livro> livros = new ArrayList<>();
 }
