530bc6ada1826be8c5623f898097e8623b8f761c
Taylam Moura <tay.moura@hotmail.com>
2025-03-22 13:10:15 -0300

    Adicionado query methods findByUsuario e findByEmail

--- src/main/java/com/reading/sala_de_leitura/repository/UsuarioRepository.java
+++ src/main/java/com/reading/sala_de_leitura/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package com.reading.sala_de_leitura.repository;
 import com.reading.sala_de_leitura.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByUsuario(String usuario);
+
+    Optional<Usuario> findByEmail(String email);
 }
