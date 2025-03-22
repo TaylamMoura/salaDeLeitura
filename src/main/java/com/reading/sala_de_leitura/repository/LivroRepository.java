adf9e7e5d3b814c633a8966f153dff0370e4d581
Taylam Moura <tay.moura@hotmail.com>
2025-03-22 13:15:54 -0300

    Adição dos metodos findByUsuario e findByIdAndUsuario

--- src/main/java/com/reading/sala_de_leitura/repository/LivroRepository.java
+++ src/main/java/com/reading/sala_de_leitura/repository/LivroRepository.java
@@ -1,7 +1,14 @@
 package com.reading.sala_de_leitura.repository;
 
 import com.reading.sala_de_leitura.entity.Livro;
+import com.reading.sala_de_leitura.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    List<Livro> findByUsuario(Usuario usuario);
+
+    Optional<Livro> findByIdAndUsuario(Long id, Usuario usuario);
 }
