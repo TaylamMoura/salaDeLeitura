01d43a9ad964cb69e5b6d47a2283739ef993f197
Taylam Moura <tay.moura@hotmail.com>
2025-04-05 12:48:16 -0300

    Alteração do securityFilterChain para adicionar configurações CORS, adicionar endpois com permissão do tipo All e desabilita função logout do Spring Security

--- src/main/java/com/reading/sala_de_leitura/security/SecurityConfig.java
+++ src/main/java/com/reading/sala_de_leitura/security/SecurityConfig.java
@@ -1,36 +1,62 @@
 package com.reading.sala_de_leitura.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private JWTFiltroAutenticacao jwtFiltro;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Desativa CSRF
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:8080"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(List.of("Content-Type", "Authorization")); // Cabeçalhos permitidos
+                    config.setAllowCredentials(true); // Permite o envio de cookies
+                    return config;
+                }))
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/usuarios", "/usuarios/login", "/inicio.html", "/css/**", "/js/**", "/cadastro.html").permitAll() // Endpoints públicos
-                        .anyRequest().authenticated() // Outros endpoints requerem autenticação
+                        .requestMatchers(
+                                "/usuarios",
+                                "/usuarios/login",
+                                "/inicio.html",
+                                "/cadastro.html",
+                                "/index.html",
+                                "/favicon.ico",
+                                "/css/**",
+                                "/js/**",
+                                "/img/**",
+                                "/logout").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Sem estado (JWT)
-                )
-                .httpBasic(Customizer.withDefaults()); // Autenticação HTTP básica
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .logout(logout -> logout.disable())
+                .addFilterBefore(jwtFiltro, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
-
