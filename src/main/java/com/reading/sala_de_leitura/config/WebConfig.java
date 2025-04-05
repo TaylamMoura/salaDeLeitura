225ec2951016ff693e525f3b5648c8ae476ff81a
Taylam Moura <tay.moura@hotmail.com>
2025-04-05 13:00:33 -0300

    Criação da classe que configura o CORS

---
+++ src/main/java/com/reading/sala_de_leitura/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.reading.sala_de_leitura.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization");
+    }
+}
