edfb085321681d8b5c55c038075d686df98d0670
Taylam Moura <tay.moura@hotmail.com>
2025-04-05 12:59:48 -0300

    Remoção de import não utilizado

--- src/main/java/com/reading/sala_de_leitura/repository/UsuarioRepository.java
+++ src/main/java/com/reading/sala_de_leitura/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package com.reading.sala_de_leitura.repository;
 
 import com.reading.sala_de_leitura.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
