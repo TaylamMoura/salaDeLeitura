812d1ce7b9fa39e7a50a5cf4d70b28fbbbe4d37c
Taylam Moura <tay.moura@hotmail.com>
2025-04-30 14:25:52 -0300

    Remoção das classes ServiceConfig.java e SessoesDeLeituraRepository.java

--- src/main/java/com/reading/sala_de_leitura/repository/SessoesDeLeituraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reading.sala_de_leitura.repository;
-
-import com.reading.sala_de_leitura.entity.SessoesDeLeitura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessoesDeLeituraRepository extends JpaRepository<SessoesDeLeitura, Long> {
-}
