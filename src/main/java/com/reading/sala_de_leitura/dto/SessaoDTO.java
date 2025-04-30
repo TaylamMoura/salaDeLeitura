2de367524ddadf9372220163b730d8fb879130a6
Taylam Moura <tay.moura@hotmail.com>
2025-04-30 14:21:10 -0300

    Criação de do Record DTO de SessaoDTO

---
+++ src/main/java/com/reading/sala_de_leitura/dto/SessaoDTO.java
@@ -0,0 +1,27 @@
+package com.reading.sala_de_leitura.dto;
+
+import com.reading.sala_de_leitura.entity.SessoesDeLeitura;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.sql.Time;
+
+public record SessaoDTO(
+        @NotNull Long usuarioId,
+        @NotNull Long livroId,
+        @Min(1) int paginasLidas,
+        String tempoLeitura,
+        int paginaFinal,
+        String urlCapa) {
+
+    public SessaoDTO(SessoesDeLeitura sessoesDeLeitura){
+
+        this(
+                sessoesDeLeitura.getUsuario().getId(),
+                sessoesDeLeitura.getLivro().getId(),
+                sessoesDeLeitura.getPaginaFinal() - sessoesDeLeitura.getPaginaInicial(), //PARA CALCULAR O TOTAL DE PÁGINAS LIDAS
+                sessoesDeLeitura.getTempoLeitura().toString(),
+                sessoesDeLeitura.getPaginaFinal(),
+                sessoesDeLeitura.getUrlCapa());
+    }
+}
