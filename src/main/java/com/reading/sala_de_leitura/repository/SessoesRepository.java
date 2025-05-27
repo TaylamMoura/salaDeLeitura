622eaee29d76de35263896fe5ffdc45fd5ec52e5
Taylam Moura <tay.moura@hotmail.com>
2025-05-27 16:49:49 -0300

    Adição do método findTopByUsuarioIdAndLivroIdOrderByInicioSessaoDesc

--- src/main/java/com/reading/sala_de_leitura/repository/SessoesRepository.java
+++ src/main/java/com/reading/sala_de_leitura/repository/SessoesRepository.java
@@ -1,10 +1,7 @@
 package com.reading.sala_de_leitura.repository;
 
-import com.reading.sala_de_leitura.entity.Livro;
 import com.reading.sala_de_leitura.entity.SessoesDeLeitura;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import java.util.Optional;
 
 public interface SessoesRepository extends JpaRepository<SessoesDeLeitura, Long> {
@@ -13,4 +10,5 @@ public interface SessoesRepository extends JpaRepository<SessoesDeLeitura, Long>
 
     Optional<SessoesDeLeitura> findTopByLivroIdOrderByFimSessaoDesc(Long livroId);
 
+    Optional<SessoesDeLeitura> findTopByUsuarioIdAndLivroIdOrderByInicioSessaoDesc(Long usuarioId, Long livroId);
 }
